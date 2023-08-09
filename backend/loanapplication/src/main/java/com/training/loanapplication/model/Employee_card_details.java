79d286c5e2e949a870930ca7f37298cc2fc494d3
unknown <jainsakshi9301@gmail.com>
2023-08-09 18:47:13 +0530

    update card details

--- backend/loanapplication/src/main/java/com/training/loanapplication/model/Employee_card_details.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/model/Employee_card_details.java
@@ -5,6 +5,7 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -12,6 +13,9 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="card_details")
 public class Employee_card_details {
+	@Id
+	@Column(name="card_details_id")
+	private int id;
 	
 	@Column(name="card_issue_date",length=10)
 	private String date;
