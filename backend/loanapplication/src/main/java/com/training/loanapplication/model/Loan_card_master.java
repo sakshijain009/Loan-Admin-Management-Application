192ab2be09b69e1af41285d39668fa51a1dfbc89
unknown <singhadia7474926rahul@gmail.com>
2023-08-09 18:50:40 +0530

    added loan card master

--- backend/loanapplication/src/main/java/com/training/loanapplication/model/Loan_card_master.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/model/Loan_card_master.java
@@ -1,5 +1,17 @@
 package com.training.loanapplication.model;
 
-public class Loan_card_master {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="loan_card_master")
+public class Loan_card_master {
+	@Column(name="card_issue_date",length=10)
+	private String date;
+	@Column(name="loan_id",length=10)
+	private String loa_nid;
+	@Column(name="employee_id",length=10)
+	private String employee_id;
+	
 }
