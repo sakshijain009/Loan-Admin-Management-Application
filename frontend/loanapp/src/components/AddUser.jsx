4636960f8de2f97bf35eaf846134a610555e9811
unknown <thesanjitanand@gmail.com>
2023-08-10 18:43:19 +0530

    integrate frontend with backend

--- frontend/loanapp/src/components/AddUser.jsx
+++ frontend/loanapp/src/components/AddUser.jsx
@@ -1,6 +1,7 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import React from 'react';
+import axios from 'axios'
 
 function AddUser(){
     const [empid, setEmpid] = React.useState("");
@@ -11,18 +12,21 @@ function AddUser(){
     const [dob, setDob] = React.useState("");
     const [doj, setDoj] = React.useState("");
 
-    // handleSubmit = async function(e){
-    //     e.preventDefault()
-    //     console.log("Form Submitted");
-    //     fetch('http://localhost:8080/addUser', {
-    //         method: 'POST',
-    //         body: JSON.stringify(body)
-    //     })
-    //     .then((response) => {
-    //         console.log(response);
-    //         return response.json();
-    //     })
-    // }
+    async function handleSubmit(e){
+        e.preventDefault()
+        console.log("Form Submitted");
+        axios.post('http://localhost:8080/addUser', {empid, name, dept, des, gender, dob, doj})
+        .then(response => console.log(response))
+        .catch(err => console.log(err));
+        // fetch('http://localhost:8080/addUser', {
+        //     method: 'POST',
+        //     body: JSON.stringify(body)
+        // })
+        // .then((response) => {
+        //     console.log(response);
+        //     return response.json();
+        // })
+    }
     return (
         <>
              <div>
@@ -33,7 +37,7 @@ function AddUser(){
           <TextField id="outlined-basic" label="Gender" variant="outlined" onChange={e => setGender(e.target.value)}/><br/>
           <TextField id="outlined-basic" label="Date of Birth (DDMMYYYY)" variant="outlined" onChange={e => setDob(e.target.value)} /><br/>
           <TextField id="outlined-basic" label="Date of Joining (DDMMYYYY)" variant="outlined" onChange={e => setDoj(e.target.value)}/><br/>
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
           </div>
         </>
     )
