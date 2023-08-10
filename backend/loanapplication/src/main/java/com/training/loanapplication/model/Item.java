1c252ab5599c22b3f4b721d5812803f9d52a5236
unknown <pulkit.g@students.iiit.ac.in>
2023-08-10 12:43:11 +0530

    updated item model

--- backend/loanapplication/src/main/java/com/training/loanapplication/model/Item.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/model/Item.java
@@ -1,8 +1,13 @@
 package com.training.loanapplication.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -76,4 +81,8 @@ public class Item {
     
     @Column(name="item_make", length=25)
     private String item_make;
+    
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(name="issue_id")
+    private List<Issue>issue;
 }
