565e68c18790b2f1717901f79ce2459d7e2bf50e
SANJIT ANAND <59444668+sanjitcodes@users.noreply.github.com>
2023-08-11 17:39:52 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button'
 import Appbar from './Appbar';
+import { useNavigate } from 'react-router-dom';
 
 import './Login.css';
 
-const Login = () => {
+const Login = ({user, loginUser}) => {
 
     const [empid, setEmpid] = useState("");
     const [pwd, setPwd] = useState("");
 
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(user!=null && user.length > 0)
+        {
+            navigate('/login');
+        }
+    },[user])
+
     async function handleSubmit(e) {
         e.preventDefault()
         console.log("Login Submit")
+        loginUser(empid);
         let resp = {empid, pwd};
         console.log(resp);
         const response = await fetch("http://localhost:8080/checkLogin", {
@@ -29,13 +41,12 @@ const Login = () => {
         });
 
         const json = await response.json();
-
         console.log(json);
     }
 
     return (
     <>
-        <Appbar />
+        <Appbar bt={"Register"}/>
             <div className='login'>
                 <h2>Login User</h2>
 
