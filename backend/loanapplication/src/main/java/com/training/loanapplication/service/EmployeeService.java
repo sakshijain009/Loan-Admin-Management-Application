7553ebfb63a8d17434fa362036deaa1f1f76ab87
unknown <pulkit.g@students.iiit.ac.in>
2023-08-11 15:04:09 +0530

    Integration of user login

--- backend/loanapplication/src/main/java/com/training/loanapplication/service/EmployeeService.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/service/EmployeeService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.training.loanapplication.dao.EmployeeRepository;
 import com.training.loanapplication.model.Employee;
 import com.training.loanapplication.model.LoginEmployee;
+import com.training.loanapplication.model.LoginResult;
 
 @Service
 public class EmployeeService {
@@ -19,7 +20,7 @@ public class EmployeeService {
 		return emp_obj;
 	}
 	
-	public String validateEmployee(LoginEmployee e)
+	public LoginResult validateEmployee(LoginEmployee e)
 	{
 		String result="";
 		Employee employee=null;
@@ -43,6 +44,8 @@ public class EmployeeService {
 				result="Login Failed";
 			}
 		}
-		return result;
+		LoginResult loginresult = new LoginResult();
+		loginresult.setCheck_login(result);
+		return loginresult;
 	}
 }
