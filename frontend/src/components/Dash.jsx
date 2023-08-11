39e3c5e8b8b7ae679271af14c9d7489aaabf5366
SANJIT ANAND <59444668+sanjitcodes@users.noreply.github.com>
2023-08-11 17:43:47 +0530

    added Dash.jsx file

---
+++ frontend/src/components/Dash.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Appbar from './Appbar'
+
+function Dash(){
+    return(
+        <>
+            <Appbar bt={"Logout"}/>
+        </>
+    )
+}
+export default Dash;
