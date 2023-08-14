5cd767ede34dc83d2a0f48f3ce9e5bbaaa055d64
unknown <pulkit.g@students.iiit.ac.in>
2023-08-14 12:53:06 +0530

    Backend Api for item Table

---
+++ backend/loanapplication/src/main/java/com/training/loanapplication/controller/ItemController.java
@@ -0,0 +1,36 @@
+package com.training.loanapplication.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.loanapplication.model.Item;
+import com.training.loanapplication.service.ItemService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class ItemController {
+	
+	@Autowired
+	ItemService itemService;
+	
+	@PostMapping("/addItem")
+	public Item saveItem(@RequestBody @Valid Item item)
+	{
+		Item i=itemService.saveItem(item);
+		return i;
+	}
+	
+	@GetMapping("/getallItems")
+	public List<Item> getallItems() 
+	{
+		return itemService.getallItems();
+	}
+}
