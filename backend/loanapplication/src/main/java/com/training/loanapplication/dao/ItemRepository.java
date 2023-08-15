491bf371c062e2f9e7c1af17ce93e3a1a710fed9
unknown <jainsakshi9301@gmail.com>
2023-08-15 19:30:46 +0530

    add all item filters

--- backend/loanapplication/src/main/java/com/training/loanapplication/dao/ItemRepository.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/dao/ItemRepository.java
@@ -15,4 +15,7 @@ public interface ItemRepository extends JpaRepository<Item, String> {
 	
 	@Query("SELECT DISTINCT i.make FROM Item as i WHERE i.category = :category")
 	List<String> getDistinctMakesByCategory(ItemCategory category);
+	
+	@Query("SELECT i FROM Item as i WHERE i.category = :category AND i.make = :make")
+	List<Item> getItemByMakeAndCategory(ItemCategory category, String make);
 }
