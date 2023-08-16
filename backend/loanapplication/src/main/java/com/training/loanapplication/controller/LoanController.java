73195c35df53bbc9ee91bda7723c5840ff39b8d7
unknown <jainsakshi9301@gmail.com>
2023-08-16 13:15:43 +0530

    add loan model

--- backend/loanapplication/src/main/java/com/training/loanapplication/controller/LoanController.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/controller/LoanController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.training.loanapplication.model.Loan;
-import com.training.loanapplication.model.LoanSubClass;
 import com.training.loanapplication.service.LoanService;
 
 import jakarta.validation.Valid;
--- backend/loanapplication/src/main/java/com/training/loanapplication/dao/LoanRepository.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/dao/LoanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.training.loanapplication.model.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
-
+	Loan findByType();
 }
