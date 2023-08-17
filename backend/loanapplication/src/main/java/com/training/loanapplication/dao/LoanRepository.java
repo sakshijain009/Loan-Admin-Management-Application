8685ec2da2d899e2709bb724195d402109c96b95
unknown <jainsakshi9301@gmail.com>
2023-08-17 10:34:41 +0530

    get cards for employee

--- backend/loanapplication/src/main/java/com/training/loanapplication/dao/LoanRepository.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/dao/LoanRepository.java
@@ -1,5 +1,7 @@
 package com.training.loanapplication.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,7 @@ public interface LoanRepository extends JpaRepository<Loan, Integer> {
 	
 	@Query("SELECT l.loan_id from Loan as l where l.type = :type")
 	int findIdByType(ItemCategory type);
+	
+//	@Query("SELECT distinct l FROM Loan as l INNER JOIN l.card WHERE l.loan_id = :emp_id")
+//	List<Loan> findLoanByEmployeeId(String emp_id);
 }
