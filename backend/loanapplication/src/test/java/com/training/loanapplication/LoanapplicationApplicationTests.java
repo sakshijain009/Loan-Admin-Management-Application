afc3c97f14e6dc709ceff77782c2fbf1cf89e3ae
unknown <thesanjitanand@gmail.com>
2023-08-17 15:16:27 +0530

    junit app file added

--- backend/loanapplication/src/test/java/com/training/loanapplication/LoanapplicationApplicationTests.java
+++ backend/loanapplication/src/test/java/com/training/loanapplication/LoanapplicationApplicationTests.java
@@ -2,8 +2,9 @@ package com.training.loanapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages="com")
+@SpringBootTest(classes=com.training.loanapplication.EmployeeControllerTest.class)
 class LoanapplicationApplicationTests {
 
 	@Test
