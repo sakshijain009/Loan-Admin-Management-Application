ddd29f83bb56d6d5c10b37079fcb587b5f465e50
aditya-10012002 <adityakr1001@gmail.com>
2023-08-18 16:09:57 +0530

    Delete User

--- frontend/src/components/Navbar/AdminViewUser.jsx
+++ frontend/src/components/Navbar/AdminViewUser.jsx
@@ -8,6 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -35,7 +36,7 @@ const AdminViewUser = () => {
   
     useEffect(() => {
         const data = async () => {
-            const res = await fetch('http://localhost:8080/admin/getAllUser', {
+            const res = await fetch('http://localhost:8080/api/admin/getAllUser', {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -47,6 +48,29 @@ const AdminViewUser = () => {
         data();
       // setData(json)
     }, [])
+
+    const [deleteRow, setDeleteRow] = useState("");
+
+    const deleteHandler = () => {
+        console.log(deleteRow);
+        const data = async () => {
+            const res = await fetch(`http://localhost:8080/api/admin/removeEmployee/${deleteRow}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+            }})
+            const dt = await res.json();
+            console.log(dt.message);
+            // setData(dt);
+            window.location.reload();
+        }
+        data();
+    }    
+
+    useEffect(() => {
+        console.log("Emp id", deleteRow);
+        deleteHandler()
+    }, [deleteRow]);
     
     return (
       <div>
@@ -64,18 +88,22 @@ const AdminViewUser = () => {
             <StyledTableCell align="center">Gender</StyledTableCell>
             <StyledTableCell align="center">Date Of Birth</StyledTableCell>
             <StyledTableCell align="center">Date Of Joining</StyledTableCell>
+            <StyledTableCell align="center">Edit</StyledTableCell>
+            <StyledTableCell align="center">Delete</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((row) => (
             <StyledTableRow key={row.id}>
               <StyledTableCell align="center">{row.id}</StyledTableCell>
-              <StyledTableCell align="center">{row.name}</StyledTableCell>
-              <StyledTableCell align="center">{row.designation}</StyledTableCell>
-              <StyledTableCell align="center">{row.department}</StyledTableCell>
-              <StyledTableCell align="center">{row.gender}</StyledTableCell>
-              <StyledTableCell align="center">{row.dob}</StyledTableCell>
-              <StyledTableCell align="center">{row.doj}</StyledTableCell>
+              <StyledTableCell align="center">{row.name || "-"}</StyledTableCell>
+              <StyledTableCell align="center">{row.designation || "-"}</StyledTableCell>
+              <StyledTableCell align="center">{row.department || "-"}</StyledTableCell>
+              <StyledTableCell align="center">{row.gender || "-"}</StyledTableCell>
+              <StyledTableCell align="center">{row.dob || "-"}</StyledTableCell>
+              <StyledTableCell align="center">{row.doj || "-"}</StyledTableCell>
+              <StyledTableCell align="center"><Link to="/adminedituser">Edit</Link></StyledTableCell>
+              <StyledTableCell align="center"><button onClick={() => setDeleteRow(row.id)}>Delete</button></StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
