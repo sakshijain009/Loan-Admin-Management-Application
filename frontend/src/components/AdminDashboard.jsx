7a0fe05e701108fd809045514595659fc6c58960
aditya-10012002 <adityakr1001@gmail.com>
2023-08-18 12:14:58 +0530

    Routes setup

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -12,7 +12,7 @@ function AdminDashboard(){
                 <hr />
                 <h3>Admin Dash Board</h3>
                 <div className="user-dashboard__buttons">
-                    <Link to="" className="user-dashboard__button">Customer Data Management</Link>
+                    <Link to="/adminadduser" className="user-dashboard__button">Customer Data Management</Link>
                     <Link to="" className="user-dashboard__button">Loan Card Management</Link>
                     <Link to="" className="user-dashboard__button">Items Master Data</Link>
                 </div>
