cb3aa9bdec8b04a4dd65f9adf3fbf61ae4923d30
unknown <thesanjitanand@gmail.com>
2023-08-18 12:21:58 +0530

    table ui

--- frontend/src/components/ViewLoanDetails.jsx
+++ frontend/src/components/ViewLoanDetails.jsx
@@ -1,4 +1,36 @@
-import React, { useEffect, useState } from 'react'
+
+import Appbar from './Appbar'
+
+import React, {useState, useEffect} from 'react';
+
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 const ViewLoanDetails = ({user}) => {
 
@@ -26,19 +58,24 @@ const ViewLoanDetails = ({user}) => {
     const renderTable = () => {
       return data.map(user1 => {
         return (
+          <div style={{display:'flex', justifyContent:'center'}}>
+          <table>
           <tr>
             <td>{user1.loan_id}</td>
             <td>{user1.duration}</td>
             <td>{user1.type}</td>
             <td>{user1.card_id}</td>
           </tr>
+          </table>
+          </div>
         )
       })
     }
   
     return (
       <div>
-        <h1 id="title">Loans Applied</h1>
+        <Appbar/>
+        {/* <h2 id="title">Loans Applied</h2>
         <table id="users">
           <thead>
             <tr>
@@ -49,7 +86,30 @@ const ViewLoanDetails = ({user}) => {
             </tr>
           </thead>
           <tbody>{renderTable()}</tbody>
-        </table>
+        </table> */}
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Loan ID</StyledTableCell>
+            <StyledTableCell align="right">Duration</StyledTableCell>
+            <StyledTableCell align="right">Loan Type</StyledTableCell>
+            <StyledTableCell align="right">Card ID</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <StyledTableRow>
+              <StyledTableCell align="right">{row.loan_id}</StyledTableCell>
+              <StyledTableCell align="right">{row.duration}</StyledTableCell>
+              <StyledTableCell align="right">{row.type}</StyledTableCell>
+              <StyledTableCell align="right">{row.card_id}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
       </div>
     )
 }
