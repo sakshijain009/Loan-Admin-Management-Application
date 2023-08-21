78ba6c67f589dbed5bc8dfca92803dff2695c943
Sakshi Jain <jainsakshi9301@gmail.com>
2023-08-21 23:24:53 +0530

    update loan

--- backend/loanapplication/src/main/java/com/training/loanapplication/dao/ItemRepository.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/dao/ItemRepository.java
@@ -25,4 +25,7 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 	
 	@Query(nativeQuery=true, value="SELECT iss.issue_id, it.item_description, it.item_make, it.item_category, it.item_value FROM issue_details iss INNER JOIN item_master it ON it.item_id = iss.item_id WHERE iss.employee_id=?1")
 	public List<Map<String,Object>> getAllItemsByEmpId(String emp_id);
+
+	@Query("SELECT i from Item as i where i.category = :type")
+	List<Item> findAllByType(ItemCategory type);
 }
