d89cd55d55515d6083296b363492c59d0d8ed913
aditya-10012002 <adityakr1001@gmail.com>
2023-08-23 18:37:53 +0530

    Home UI

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Appbar from './Appbar'
 import { useNavigate } from 'react-router-dom';
 import './dashboard.css';
+import { Card } from 'react-bootstrap';
 
 function Dashboard(){
     
@@ -21,9 +22,29 @@ function Dashboard(){
 
     return (
         <>
-            <Appbar hbtn={"0"} bt={"Register"}/>
+            <Appbar hbtn={"/"} bt={"Register"}/>
             <div className="dashboard-container">
-                <div className="dashboard-card">
+
+
+            <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Admin</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Login as Admin</Card.Subtitle>
+        <Card.Link onClick={adminLogin}>Login</Card.Link>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Employee</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Login/Register as Employee</Card.Subtitle>
+        <Card.Link onClick={userRegister}>Register</Card.Link>
+        <Card.Link onClick={userLogin}>Login</Card.Link>
+      </Card.Body>
+    </Card>
+
+
+                {/* <div className="dashboard-card">
                     <span>Admin Login</span>
                     <button className='dashboard-button' onClick={adminLogin}>Login</button>
                 </div>
@@ -33,7 +54,7 @@ function Dashboard(){
                     <button className='dashboard-button' onClick={userRegister}>Register</button>
                     <button className='dashboard-button' onClick={userLogin}>Login</button>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
