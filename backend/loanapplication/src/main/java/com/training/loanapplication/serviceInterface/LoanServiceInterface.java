c53c9600ecd882a864d0f696b639dcecebfd07ab
unknown <pulkit.g@students.iiit.ac.in>
2023-08-25 13:31:31 +0530

    updated getallLoans

--- backend/loanapplication/src/main/java/com/training/loanapplication/serviceInterface/LoanServiceInterface.java
+++ backend/loanapplication/src/main/java/com/training/loanapplication/serviceInterface/LoanServiceInterface.java
@@ -13,6 +13,6 @@ import jakarta.validation.Valid;
 public interface LoanServiceInterface {
 	public Message saveLoan(@Valid Loan loan) throws DuplicateEntryException;
 	public Loan getLoanById(int loan_id) throws ResourceNotFoundException ;
-	public List<Map<String,Object>> getAllLoans(Map<String, String> header) throws ResourceNotFoundException;
+	public List<Map<String,Object>> getAllLoans(String emp_id) throws ResourceNotFoundException;
 	public List<String> getAllTypes() throws ResourceNotFoundException;
 }
