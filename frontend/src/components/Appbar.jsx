91683ebee70a9e8f4f7c3f9b30032811b76d9bb4
aditya-10012002 <adityakr1001@gmail.com>
2023-08-26 11:21:19 +0530

    Navbar Issues fix

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,51 +1,77 @@
-import React, {useEffect} from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { useNavigate } from 'react-router-dom';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import { Link } from 'react-router-dom';
-import { palette } from '@mui/system';
+import React, { useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { useNavigate } from "react-router-dom";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import { Link } from "react-router-dom";
+import { palette } from "@mui/system";
 
-export default function Appbar({hbtn, bt}) {
+export default function Appbar({ hbtn, bt }) {
   const navigate = useNavigate();
-  const [prof,setProf] = React.useState("");
-  useEffect( ()=> {
+  const [prof, setProf] = React.useState("");
+  useEffect(() => {
     console.log(prof);
-    if(sessionStorage.getItem("username") != null) {
+    if (sessionStorage.getItem("username") != null) {
       setProf(sessionStorage.getItem("username"));
-    } 
-    if(sessionStorage.getItem("admin") != null) {
+    }
+    if (sessionStorage.getItem("admin") != null) {
       const p = JSON.parse(sessionStorage.getItem("admin")).username;
       setProf(p);
-    } 
-  }, [prof])
+    }
+  }, [prof]);
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{backgroundColor:"#4477CE"}}>
+      <AppBar position="static" style={{ backgroundColor: "#4477CE" }}>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}            
-            onClick={() => (navigate(hbtn))}
+            sx={{ mr: 2 }}
+            onClick={() => navigate(hbtn)}
           >
-          <HomeRoundedIcon />
+            <HomeRoundedIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, paddingLeft:1 }}>
-            Loan Management 
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, paddingLeft: 1 }}
+          >
+            Loan Management
           </Typography>
-          <div style={{display:'flex', justifyContent:'center', marginRight:30}}>
-          <Box component="span" sx={{flexGrow:1, p:1, borderRadius:8}} className="bg-light text-primary">
-            {prof && <Typography variant="h9" className="font-weight-bold px-2">Welcome {prof}</Typography> }
-          </Box>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginRight: 30,
+            }}
+          >
+            {prof && (
+              <Box
+                component="span"
+                sx={{ flexGrow: 1, p: 1, borderRadius: 8 }}
+                className="bg-light text-primary"
+              >
+                <Typography variant="h9" className="font-weight-bold px-2">
+                  Welcome {prof}
+                </Typography>
+              </Box>
+            )}
           </div>
-          <Button variant="outlined" color="inherit" onClick={()=>(navigate(`/${bt}`))}>{bt}</Button>
+          {bt && (
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={() => navigate(`/${bt}`)}
+            >
+              {bt}
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
