595c8d5354dbd70faded7a38e96b2d24fc8a7dca
aditya-10012002 <adityakr1001@gmail.com>
2023-08-28 11:42:08 +0530

    Loan Item edit UI

--- frontend/src/components/AdminEditItem.jsx
+++ frontend/src/components/AdminEditItem.jsx
@@ -1,149 +1,190 @@
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button'
-import React from 'react';
-import Appbar from './Appbar';
-import '../Pages/Register.css';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React from "react";
+import Appbar from "./Appbar";
+import "../Pages/Register.css";
 
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-import { useState, useEffect } from 'react';
-// import {Link, useNavigate, useParams} from 'react-router-dom';
-import { InputAdornment } from '@mui/material';
-import { Modal } from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from "react";
+import { InputAdornment } from "@mui/material";
+import { Modal } from "react-bootstrap";
+import CloseIcon from "@mui/icons-material/Close";
+import SendIcon from "@mui/icons-material/Send";
 
-function AdminEditItem({id, show, handleClose, setEditDone}) {
-    // const navigate = useNavigate();
-    const [itemId, setItemId] = React.useState("");
-    const [category, setCategory] = React.useState("");
-    const [make, setMake] = React.useState("");
-    const [description, setDescription] = React.useState("");
-    const [value, setValue] = React.useState("");
-    const [status, setStatus] = useState("");
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-    const [error, setError] = React.useState({
-        "make":'',
-        "value":''
+function AdminEditItem({ id, show, handleClose, setEditDone }) {
+  // const navigate = useNavigate();
+  const [itemId, setItemId] = React.useState("");
+  const [category, setCategory] = React.useState("");
+  const [make, setMake] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [value, setValue] = React.useState("");
+  const [status, setStatus] = useState("");
+
+  const [error, setError] = React.useState({
+    make: "",
+    value: "",
+  });
+
+  // const {id} = useParams();
+
+  useEffect(() => {
+    const data = async () => {
+      console.log(id);
+      const response = await fetch(
+        `http://localhost:8080/api/admin/getItemById/${id}`
+      );
+      const res = await response.json();
+      setItemId(res.item_id);
+      setCategory(res.category);
+      setMake(res.make);
+      setDescription(res.description);
+      setValue(res.value);
+      setStatus(res.status);
+    };
+    data();
+  }, [id]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setError({
+      make: make ? "" : "Item Make is required",
+      value: value !== 0 ? "" : "Value is required",
     });
-
-    // const {id} = useParams();
-
-    useEffect(() => {
-        const data = async () => {
-            console.log(id);
-            const response = await fetch(`http://localhost:8080/api/admin/getItemById/${id}`);
-            const res = await response.json();
-            setItemId(res.item_id);
-            setCategory(res.category);
-            setMake(res.make);
-            setDescription(res.description);
-            setValue(res.value);
-            setStatus(res.status);
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/admin/updateItem",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            item_id: itemId,
+            category: category,
+            make: make,
+            description: description,
+            value: value,
+            status: status,
+          }),
         }
-        data();
-    }, [id])
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        setError({
-            "make": make ? "" : "Item Make is required",
-            "value": value !== 0 ? "" : "Value is required"
-        })
-        try {
-        const response = await fetch("http://localhost:8080/api/admin/updateItem", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(
-                {
-                    "item_id": itemId,
-                    "category": category,
-                    "make": make,
-                    "description": description,
-                    "value": value,
-                    "status": status
-                }
-            )
-        });
-        const json = await response.json();
-        console.log(json);
-        console.log(response.status);
-        if(response.status === 200){
-            // navigate('/adminviewitem');
-            handleClose();
-            setEditDone(prev => !prev);
-            toast(json.message);
-            // alert(json.message);
-            // window.location.reload();
-        }
-        else{
-            toast(json.message);
-        }
-            
+      );
+      const json = await response.json();
+      console.log(json);
+      console.log(response.status);
+      if (response.status === 200) {
+        // navigate('/adminviewitem');
+        handleClose();
+        setEditDone((prev) => !prev);
+        toast(json.message);
+        // alert(json.message);
+        // window.location.reload();
+      } else {
+        toast(json.message);
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-    }
+  }
 
-    return (
-        // <>
-        //     <Appbar/>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Item</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div className='modal_register'>
-                <h2>Edit Item</h2>
-                <TextField className='text_register' disabled label='Item ID' variant='outlined' InputProps={{
-        endAdornment: <InputAdornment position="end">{itemId}</InputAdornment>,
-                    }} />
-                <TextField id="outlined-basic" label="Category" variant='outlined' disabled className='text_register' value={category}
-                    onChange={
-                        e => setCategory(e.target.value)
-                    }/>
-                <TextField id="outlined-basic" label="Item Make" variant='outlined' disabled  className='text_register' value={make}
-                    onChange={
-                        e => setMake(e.target.value)
-                    }/>
-                    {error.make && <p style={{color:'red'}}>{error.make}</p>}
-                    <TextField id="outlined-basic" label="Description" variant='outlined'  className='text_register' value={description}
-                    onChange={
-                        e => setDescription(e.target.value)
-                    }/>
-                    <TextField id="outlined-basic" label="Value" variant='outlined'  className='text_register' value={value}
-                    onChange={
-                        e => setValue(e.target.value)
-                    }/>
-                    {error.value && <p style={{color:'red'}}>{error.value}</p>}
-                    <TextField id="outlined-basic" label="Status" variant='outlined' disabled className='text_register' value={status}
-                    onChange={
-                        e => setStatus(e.target.value)
-                    }/>
+  return (
+    // <>
+    //     <Appbar/>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Item</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="modal_register">
+          <h2>Edit Item</h2>
+          <TextField
+            className="text_register"
+            disabled
+            label="Item ID"
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">{itemId}</InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Category"
+            variant="outlined"
+            disabled
+            className="text_register"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Item Make"
+            variant="outlined"
+            disabled
+            className="text_register"
+            value={make}
+            onChange={(e) => setMake(e.target.value)}
+          />
+          {error.make && <p style={{ color: "red" }}>{error.make}</p>}
+          <TextField
+            id="outlined-basic"
+            label="Description"
+            variant="outlined"
+            className="text_register"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Value"
+            variant="outlined"
+            className="text_register"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          {error.value && <p style={{ color: "red" }}>{error.value}</p>}
+          <TextField
+            id="outlined-basic"
+            label="Status"
+            variant="outlined"
+            disabled
+            className="text_register"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+          />
 
-
-                {/* <Button variant="contained" className='register_button'
+          {/* <Button variant="contained" className='register_button'
                     onClick={handleSubmit}>Update Item</Button> */}
-            </div>
-            <ToastContainer/>
-            </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            Update Item
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      
-        // </>
-    )
+        </div>
+        <ToastContainer />
+      </Modal.Body>
+      <Modal.Footer className="d-flex flex-row justify-content-end gap-2">
+        <Button
+          variant="contained"
+          onClick={handleClose}
+          startIcon={<CloseIcon />}
+        >
+          Close
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          endIcon={<SendIcon />}
+        >
+          Update Item
+        </Button>
+      </Modal.Footer>
+    </Modal>
+
+    // </>
+  );
 }
 export default AdminEditItem;
--- frontend/src/components/AdminEditLoan.jsx
+++ frontend/src/components/AdminEditLoan.jsx
@@ -1,136 +1,158 @@
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button'
-import React from 'react';
-import '../Pages/Register.css'
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React from "react";
+import "../Pages/Register.css";
 
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
-import { useState, useEffect } from 'react';
-import { InputAdornment } from '@mui/material';
-import { Modal, Toast } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { InputAdornment } from "@mui/material";
+import { Modal } from "react-bootstrap";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import CloseIcon from "@mui/icons-material/Close";
+import SendIcon from "@mui/icons-material/Send";
 
-function AdminEditUser({id, show, handleClose, setEditDone}) {
-    // const navigate = useNavigate();
-    const [loan_id, setLoan_id] = React.useState("");
-    const [type, setType] = React.useState("");
-    const [duration, setDuration] = React.useState(0);
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-    const [error, setError] = React.useState({
-        "duration": ''
+function AdminEditUser({ id, show, handleClose, setEditDone }) {
+  // const navigate = useNavigate();
+  const [loan_id, setLoan_id] = React.useState("");
+  const [type, setType] = React.useState("");
+  const [duration, setDuration] = React.useState(0);
+
+  const [error, setError] = React.useState({
+    duration: "",
+  });
+
+  // const {id} = useParams();
+
+  useEffect(() => {
+    const data = async () => {
+      // console.log(id);
+      const response = await fetch(
+        `http://localhost:8080/api/admin/getLoanById/${id}`
+      );
+      const res = await response.json();
+      setLoan_id(res.loan_id);
+      setType(res.type);
+      setDuration(res.duration);
+    };
+    data();
+  }, [loan_id]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setError({
+      duration: duration > 0 ? "" : "Please enter a valid duration",
     });
- 
-    // const {id} = useParams();
-
-    useEffect(() => {
-        const data = async () => {
-            // console.log(id);
-            const response = await fetch(`http://localhost:8080/api/admin/getLoanById/${id}`);
-            const res = await response.json();
-            setLoan_id(res.loan_id);
-            setType(res.type);
-            setDuration(res.duration);
+    try {
+      if (duration <= 0) {
+        throw new Error("Please enter a valid duration");
+      }
+      // console.log("Registration successful");
+      const response = await fetch(
+        "http://localhost:8080/api/admin/updateLoan",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            loan_id: loan_id,
+            type: type,
+            duration: duration,
+          }),
         }
-        data();
-    }, [loan_id])
-
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-
-        setError({
-            "duration": duration > 0 ? "" : "Please enter a valid duration"
-        })
-        try {
-    
-            if(duration <= 0) {
-                throw new Error("Please enter a valid duration");
-            }
-        // console.log("Registration successful");
-        const response = await fetch("http://localhost:8080/api/admin/updateLoan", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(
-                {
-                    "loan_id": loan_id,
-                    "type": type,
-                    "duration": duration,
-                }
-            )
-        });
-        const json = await response.json();
-        console.log(json);
-        console.log(response.status);
-        if(response.status === 200){
-            // navigate('/adminviewuser');
-            handleClose();
-            setEditDone(prev => !prev);
-            toast(json.message);
-            // alert(json.message);
-            // window.location.reload();
-        }
-        else{
-            toast(json.message);
-        }
-            
-    } catch (error) {
+      );
+      const json = await response.json();
+      console.log(json);
+      console.log(response.status);
+      if (response.status === 200) {
+        // navigate('/adminviewuser');
+        handleClose();
+        setEditDone((prev) => !prev);
         toast(json.message);
+        // alert(json.message);
+        // window.location.reload();
+      } else {
+        toast(json.message);
+      }
+    } catch (error) {
+      toast(json.message);
     }
-    }
+  }
 
-    return (
-        // <>
-        //     <Appbar/>
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Loan</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div className='modal_register'>
-                {/* <h2>Edit Loan</h2> */}
-                <TextField className='text_register' disabled placeholder='Loan ID' InputProps={{
-        endAdornment: <InputAdornment position="end">{loan_id}</InputAdornment>,
-                    }} />
-                <TextField id="outlined-basic" disabled label='Loan Type' variant='outlined' className='text_register' value={type}
-                    onChange={
-                        e => setType(e.target.value)
-                    }/>
-                <TextField id="outlined-basic" label="Duration" variant='outlined' className='text_register' value={duration}
-                    type='number'
-                    onChange={
-                        e => setDuration(e.target.value || "0")
-                    }/>
-                    {error.duration && <p style={{color:'red'}}>{error.duration}</p>}
-                <Box sx={
-                    {
-                        minWidth: 120,
-                        paddingTop: 1
-                    }
-                }>
-                    
-                </Box>
+  return (
+    // <>
+    //     <Appbar/>
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Edit Loan</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="modal_register">
+          {/* <h2>Edit Loan</h2> */}
+          <TextField
+            className="text_register"
+            disabled
+            placeholder="Loan ID"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">{loan_id}</InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            disabled
+            label="Loan Type"
+            variant="outlined"
+            className="text_register"
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Duration"
+            variant="outlined"
+            className="text_register"
+            value={duration}
+            type="number"
+            onChange={(e) => setDuration(e.target.value || "0")}
+          />
+          {error.duration && <p style={{ color: "red" }}>{error.duration}</p>}
+          <Box
+            sx={{
+              minWidth: 120,
+              paddingTop: 1,
+            }}
+          ></Box>
 
-      
-
-                {/* <Button variant="contained" className='register_button'
+          {/* <Button variant="contained" className='register_button'
                     onClick={handleSubmit}>Update Loan</Button> */}
-            </div>
-            <ToastContainer />
-            </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSubmit}>
-            Update Loan
-          </Button>
-        </Modal.Footer>
-      </Modal>
-        // </>
-    )
+        </div>
+        <ToastContainer />
+      </Modal.Body>
+      <Modal.Footer className="d-flex flex-row justify-content-end gap-2">
+        <Button
+          variant="contained"
+          onClick={handleClose}
+          startIcon={<CloseIcon />}
+        >
+          Close
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          endIcon={<SendIcon />}
+        >
+          Update Loan
+        </Button>
+      </Modal.Footer>
+    </Modal>
+    // </>
+  );
 }
 export default AdminEditUser;
