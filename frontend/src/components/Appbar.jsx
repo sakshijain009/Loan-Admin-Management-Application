69bb067b33d15dda53a2870aee2a109b845d76c7
unknown <thesanjitanand@gmail.com>
2023-08-28 15:17:41 +0530

    added user profile popover

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -8,10 +8,12 @@ import IconButton from "@mui/material/IconButton";
 import { useNavigate } from "react-router-dom";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import PersonIcon from "@mui/icons-material/Person";
+import Popover from '@mui/material/Popover';
 
 export default function Appbar({ hbtn, bt }) {
   const navigate = useNavigate();
   const [prof, setProf] = React.useState("");
+  const [det, setDet] = React.useState([]);
   useEffect(() => {
     console.log(prof);
     if (sessionStorage.getItem("username") != null) {
@@ -24,6 +26,7 @@ export default function Appbar({ hbtn, bt }) {
         const res = await response.json();
         if (response.status === 200) {
           setProf(res.name);
+          setDet(res);
         } else {
           setProf(id);
         }
@@ -35,6 +38,20 @@ export default function Appbar({ hbtn, bt }) {
       setProf(p);
     }
   }, [prof]);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClickProf = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseProf = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: "#4477CE" }}>
@@ -69,11 +86,30 @@ export default function Appbar({ hbtn, bt }) {
                 component="span"
                 sx={{ flexGrow: 1, p: 1, borderRadius: 8 }}
                 className="bg-light text-dark"
+               
               >
-                <Typography variant="h9" className="font-weight-bold px-2">
+                <Typography variant="h9" className="font-weight-bold px-2"  onClick={handleClickProf}>
                   <PersonIcon />
                   Welcome {prof}
                 </Typography>
+                
+                <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleCloseProf}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              sx={{padding:'10px'}}
+              >
+              <Typography sx={{p:0.5}}><b>Designation</b> {det.designation}</Typography>
+              <Typography sx={{p:0.5 }}><b>Department</b> {det.department}</Typography>
+              <Typography sx={{ p: 0.5 }}><b>Gender</b> {det.gender==='M'?'Male':(det.gender==='F'?'Female':'Others')}</Typography>
+              <Typography sx={{ p: 0.5 }}><b>Date of Birth</b> {det.dob}</Typography>
+              <Typography sx={{ p: 0.5 }}><b>Date of Joining</b> {det.doj}</Typography>
+            </Popover>
               </Box>
             )}
           </div>
